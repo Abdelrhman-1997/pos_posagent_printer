43548bc08bfc97c78a442a30781e609ac0597957
Abdelrhman Ashraf <124148126+Abdelrhman-1997@users.noreply.github.com>
2026-08-01 02:01:52 +0300

    Add POSAgent configuration fields

---
+++ pos_posagent/models/pos_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    use_posagent = fields.Boolean(string="POS Agent")
+    pos_agent_port = fields.Integer(string="Proxy Port", default=9069)
+    posagent_enable_printer = fields.Boolean(string="Receipt Printer")
+    posagent_enable_cashdrawer = fields.Boolean(string="Cash Drawer")
